cd9148e0e2c5509d4216adb129bb6dfdc1e942dd
k.kovalevskii <k.kovalevskii@brandquad.ru>
2021-09-19 23:58:34 +0300

    fix css selectors in locators and browser method in login page

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,6 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CLASS_NAME, "col-sm-6.login_form1")
-    REGISTER_FORM = (By.CLASS_NAME, "col-sm-6.register_form1")
+    LOGIN_FORM = (By.CLASS_NAME, "col-sm-6.login_form")
+    REGISTER_FORM = (By.CLASS_NAME, "col-sm-6.register_form")
     
--- pages/login_page.py
+++ pages/login_page.py
@@ -11,7 +11,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in browser.current_url, "Login sublink is not presented in URL"
+        assert "login" in self.browser.current_url, "Login sublink is not presented in URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
