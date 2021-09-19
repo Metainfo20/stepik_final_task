24cefe348256910635ddaebb5e5bf79f85e9d09e
k.kovalevskii <k.kovalevskii@brandquad.ru>
2021-09-19 23:51:01 +0300

    refactoring files

--- pages/locators.py
+++ pages/locators.py
@@ -5,11 +5,6 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    LOGIN_EMAIL = (By.NAME, "login-username")
-    LOGIN_PASS = (By.NAME, "login-password")
-    LOGIN_BUTTON = (By.NAME, "login_submit")
-    REGISTER_EMAIL = (By.NAME, "registration-email")
-    REGISTER_PASS = (By.NAME, "registration-password1")
-    REGISTER_PASS_COMFIRM = (By.NAME, "registration-password2")
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
+    LOGIN_FORM = (By.CLASS_NAME, "col-sm-6.login_form1")
+    REGISTER_FORM = (By.CLASS_NAME, "col-sm-6.register_form1")
     
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,5 +8,13 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()      # проверка ссылки на логин
-    page.should_be_login_page()      # проверка отображения страницы логина
 
+
+def test_guest_can_see_register_and_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_page()      # проверка отображения страницы логина
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
