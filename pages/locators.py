eafe8452ee650afc309a1691beb77b12a3c3c62e
k.kovalevskii <k.kovalevskii@brandquad.ru>
2021-09-19 22:44:29 +0300

    add locators for login/reg page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.NAME, "login-username")
+    LOGIN_PASS = (By.NAME, "login-password")
+    LOGIN_BUTTON = (By.NAME, "login_submit")
+    REGISTER_EMAIL = (By.NAME, "registration-email")
+    REGISTER_PASS = (By.NAME, "registration-password1")
+    REGISTER_PASS_COMFIRM = (By.NAME, "registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
+    
