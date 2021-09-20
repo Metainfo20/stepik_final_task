05c3f9f15ed6b142bba18d8e978674ad05085eff
k.kovalevskii <k.kovalevskii@brandquad.ru>
2021-09-20 15:56:48 +0300

    add new locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,7 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CLASS_NAME, "col-sm-6.login_form")
     REGISTER_FORM = (By.CLASS_NAME, "col-sm-6.register_form")
-    
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    BASKET = (By.CLASS_NAME, "not_exist")
